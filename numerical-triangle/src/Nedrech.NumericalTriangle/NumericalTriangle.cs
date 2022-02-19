f9caead071c6031bb21242fbd36fb56624d4be5f
nedrech <denischernyy@ya.ru>
2022-02-19 20:39:01 +0300

    Fixed method validation, added test, description of the sum method

--- numerical-triangle/src/Nedrech.NumericalTriangle/NumericalTriangle.cs
+++ numerical-triangle/src/Nedrech.NumericalTriangle/NumericalTriangle.cs
@@ -4,11 +4,15 @@ public class NumericalTriangle
 {
     private readonly double[][] _levels;
 
+    private readonly Lazy<double> _maxSum;
+
+    public double MaxSum => _maxSum.Value;
+
     public NumericalTriangle(double[][] levels)
     {
         _levels = levels;
-        
         EnsureValid();
+        _maxSum = new Lazy<double>(CalcMaxSum);
     }
 
     /// <summary>
@@ -17,8 +21,18 @@ public class NumericalTriangle
     /// <exception cref="ArgumentException">It is thrown out if the level is incorrect</exception>
     private void EnsureValid()
     {
-        for (int i = 1; i <= _levels.Length; i++)
-            if (i != _levels[i].Length)
+        for (int i = 0; i < _levels.Length; i++)
+            if (i + 1 != _levels[i].Length)
                 throw new ArgumentException("Incorrect order of levels.");
+    }
+
+    /// <summary>
+    /// The function finds the maximum sum of the triangle numbers in depth.
+    /// </summary>
+    /// <returns>Maximum sum of the triangle numbers.</returns>
+    private double CalcMaxSum()
+    {
+        
+        return 0;
     }
 }
--- numerical-triangle/test/Nedrech.NumericalTriangle.Tests/NumericalTriangleTests.cs
+++ numerical-triangle/test/Nedrech.NumericalTriangle.Tests/NumericalTriangleTests.cs
@@ -20,4 +20,22 @@ public class NumericalTriangleTests
         // Assert
         Assert.Throws<ArgumentException>(() => new NumericalTriangle(levels));
     }
+
+    [Fact]
+    public void Should_Create_Object()
+    {
+        // Arrange
+        double[][] levels = {
+            new[] {1.0},
+            new[] {1.0, 1},
+            new[] {1.0, 1, 1},
+            new[] {1.0, 1, 1, 1}
+        };
+        
+        // Act
+        NumericalTriangle nt = new(levels);
+        
+        // Assert
+        Assert.NotNull(nt);
+    }
 }

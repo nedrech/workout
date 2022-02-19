08c07a6273b5a182908f1ddff6096c4ea0c5c1ff
nedrech <denischernyy@ya.ru>
2022-02-19 20:23:57 +0300

    Validating the correct NumericalTriangle

--- numerical-triangle/src/Nedrech.NumericalTriangle/NumericalTriangle.cs
+++ numerical-triangle/src/Nedrech.NumericalTriangle/NumericalTriangle.cs
@@ -2,5 +2,23 @@
 
 public class NumericalTriangle
 {
-    
+    private readonly double[][] _levels;
+
+    public NumericalTriangle(double[][] levels)
+    {
+        _levels = levels;
+        
+        EnsureValid();
+    }
+
+    /// <summary>
+    /// The function checks the correctness of the triangle levels.
+    /// </summary>
+    /// <exception cref="ArgumentException">It is thrown out if the level is incorrect</exception>
+    private void EnsureValid()
+    {
+        for (int i = 1; i <= _levels.Length; i++)
+            if (i != _levels[i].Length)
+                throw new ArgumentException("Incorrect order of levels.");
+    }
 }

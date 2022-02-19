511f53515f58b57ee62936cfb0579475756e00f9
nedrech <denischernyy@ya.ru>
2022-02-19 20:30:29 +0300

    Added test for validation

--- numerical-triangle/test/Nedrech.NumericalTriangle.Tests/NumericalTriangleTests.cs
+++ numerical-triangle/test/Nedrech.NumericalTriangle.Tests/NumericalTriangleTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Xunit;
 
 namespace Nedrech.NumericalTriangle.Tests;
@@ -5,7 +6,18 @@ namespace Nedrech.NumericalTriangle.Tests;
 public class NumericalTriangleTests
 {
     [Fact]
-    public void Test1()
+    public void Should_Throws_On_Incorrect_Argument()
     {
+        // Arrange
+        double[][] levels = {
+            new[] {1.0},
+            new[] {1.0, 1},
+            new[] {1.0, 1},
+            new[] {1.0, 1, 1, 1}
+        };
+        
+        // Act
+        // Assert
+        Assert.Throws<ArgumentException>(() => new NumericalTriangle(levels));
     }
 }

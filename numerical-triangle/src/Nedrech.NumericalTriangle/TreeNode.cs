f2037481e1c4efd1a642205c9f732d27d88deb65
nedrech <denischernyy@ya.ru>
2022-02-20 12:53:25 +0300

    Added TreeNode class and test for it

---
+++ numerical-triangle/src/Nedrech.NumericalTriangle/TreeNode.cs
@@ -0,0 +1,32 @@
+﻿namespace Nedrech.NumericalTriangle;
+
+/// <summary>
+/// A class for describing a double binary tree.
+/// </summary>
+public class TreeNode
+{
+    public double Data { get; private init; }
+
+    public TreeNode? Left { get; private init; }
+
+    public TreeNode? Right { get; private init; }
+
+    /// <summary>
+    /// A method for creating a binary tree.
+    /// </summary>
+    /// <param name="levels">Binary tree in the form of a 2d array.</param>
+    /// <returns>Binary tree.</returns>
+    public static TreeNode CreateTree(double[][] levels) =>
+        InsertLevel(levels)!;
+
+    /// <summary>
+    /// Recursive method for creating a binary tree.
+    /// </summary>
+    private static TreeNode? InsertLevel(double[][] arr, TreeNode? parent = null, int i = 0, int j = 0) =>
+        i >= arr.Length ? parent : new TreeNode
+        {
+            Data = arr[i][j],
+            Left = InsertLevel(arr, parent?.Left, i + 1, j),
+            Right = InsertLevel(arr, parent?.Right, i + 1, j + 1)
+        };
+}
---
+++ numerical-triangle/test/Nedrech.NumericalTriangle.Tests/TreeNodeTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace Nedrech.NumericalTriangle.Tests;
+
+public class TreeNodeTests
+{
+    [Fact]
+    public void Should_Create_Object()
+    {
+        // Arrange
+        double[][] levels = {
+            new[] {1.0},
+            new[] {2.0, 3},
+            new[] {4.0, 5, 6}
+        };
+        
+        // Act
+        TreeNode tn = TreeNode.CreateTree(levels);
+        
+        // Assert
+        Assert.Equal(1, tn.Data);
+        Assert.Equal(2, tn.Left?.Data);
+        Assert.Equal(3, tn.Right?.Data);
+        Assert.Equal(4, tn.Left?.Left?.Data);
+        Assert.Equal(5, tn.Left?.Right?.Data);
+        Assert.Equal(5, tn.Right?.Left?.Data);
+        Assert.Equal(6, tn.Right?.Right?.Data);
+    }
+}
